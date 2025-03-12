7c4436cc17409e0e8589a7243823be7407c82106
jadson-js <jadson20051965@gmail.com>
2025-03-12 13:23:29 -0300

    sidebar in component

--- src/layout/RootLayout.tsx
+++ src/layout/RootLayout.tsx
@@ -1,10 +1,7 @@
 import React from "react";
 import Header from "../components/navbar/Navbar";
-import { NavLink, Outlet } from "react-router-dom";
-import { AiOutlineDashboard } from "react-icons/ai";
-import { MdDisplaySettings } from "react-icons/md";
-import { SiGoogleanalytics } from "react-icons/si";
-import { FaRankingStar } from "react-icons/fa6";
+import Sidebar from "../components/sidebar/Sidebar";
+import { Outlet } from "react-router-dom";
 import "./styles.css";
 
 const RootLayout = () => {
@@ -13,37 +10,7 @@ const RootLayout = () => {
       <Header />
       <div className="container-bg">
         <div className="container layout">
-          <nav className="nav">
-            <ul>
-              <NavLink className="nav-link" to={"/Dashboard"}>
-                <li>
-                  <AiOutlineDashboard className="link-icon" size={14} />
-                  <span className="font-xs">Dashboard</span>
-                </li>
-              </NavLink>
-
-              <NavLink className="nav-link" to={"/GestaoReparos"}>
-                <li>
-                  <MdDisplaySettings className="link-icon" size={14} />
-                  <span className="font-xs">Gestão De Reparos</span>
-                </li>
-              </NavLink>
-
-              <NavLink className="nav-link" to={"/AnalitycLogs"}>
-                <li>
-                  <SiGoogleanalytics className="link-icon" size={14} />
-                  <span className="font-xs">Análises De Ocorrência</span>
-                </li>
-              </NavLink>
-
-              <NavLink className="nav-link" to={"/Ranking"}>
-                <li>
-                  <FaRankingStar className="link-icon" size={14} />
-                  <span className="font-xs">Ranking</span>
-                </li>
-              </NavLink>
-            </ul>
-          </nav>
+          <Sidebar />
           <Outlet />
         </div>
       </div>
