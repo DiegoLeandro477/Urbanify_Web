4c270f89d06c21f70c48c19bf5da0792ea89a98c
jadson-js <jadson20051965@gmail.com>
2025-03-13 07:55:05 -0300

    news data to test

--- data.js
+++ data.js
@@ -41,4 +41,173 @@ export const markers = [
     },
     street: "Rua Machado De Assis",
   },
+  {
+    district: "Liberdade",
+    created_at: "2025-03-13T07:51:00.000Z",
+    falseId: "2025-03-13T07:51:00.000Z",
+    status: 0,
+    geohash: "7p8986c",
+    subregion: "São Luís",
+    childrens: [
+      {
+        severity: 1,
+        created_at: "2025-03-13T07:51:00.000Z",
+      },
+    ],
+    address: "São Luís_Liberdade",
+    id: "generated_1",
+    coordinates: {
+      latitude: "-2.5332",
+      longitude: "-44.2844",
+    },
+    street: "Rua Machado De Assis",
+  },
+  {
+    district: "Liberdade",
+    created_at: "2025-03-13T07:51:00.000Z",
+    falseId: "2025-03-13T07:51:00.000Z",
+    status: 0,
+    geohash: "7p8986c",
+    subregion: "São Luís",
+    childrens: [
+      {
+        severity: 1,
+        created_at: "2025-03-13T07:51:00.000Z",
+      },
+    ],
+    address: "São Luís_Liberdade",
+    id: "generated_2",
+    coordinates: {
+      latitude: "-2.5350",
+      longitude: "-44.2832",
+    },
+    street: "Rua Machado De Assis",
+  },
+  {
+    district: "Liberdade",
+    created_at: "2025-03-13T07:51:00.000Z",
+    falseId: "2025-03-13T07:51:00.000Z",
+    status: 0,
+    geohash: "7p8986c",
+    subregion: "São Luís",
+    childrens: [
+      {
+        severity: 1,
+        created_at: "2025-03-13T07:51:00.000Z",
+      },
+    ],
+    address: "São Luís_Liberdade",
+    id: "generated_3",
+    coordinates: {
+      latitude: "-2.5373",
+      longitude: "-44.2833",
+    },
+    street: "Rua Machado De Assis",
+  },
+  {
+    district: "Liberdade",
+    created_at: "2025-03-07T11:45:46.338Z",
+    falseId: "2025-03-07T11:45:46.338Z",
+    status: 0,
+    geohash: "7p8986c",
+    subregion: "São Luís",
+    childrens: [
+      {
+        severity: 1,
+        created_at: "2025-03-07T11:45:46.338Z",
+      },
+    ],
+    address: "São Luís_Liberdade",
+    id: "laJ3UaUxUHtE5f8O",
+    coordinates: {
+      latitude: "-2.5325999611122065",
+      longitude: "-44.284021668688126",
+    },
+    street: "Rua Machado De Assis",
+  },
+  {
+    district: "Liberdade",
+    created_at: "2025-03-07T11:45:46.338Z",
+    falseId: "2025-03-07T11:45:46.338Z",
+    status: 0,
+    geohash: "7p8986c",
+    subregion: "São Luís",
+    childrens: [
+      {
+        severity: 1,
+        created_at: "2025-03-07T11:45:46.338Z",
+      },
+    ],
+    address: "São Luís_Liberdade",
+    id: "laJ3UaUxUHtE5f8O",
+    coordinates: {
+      latitude: "-2.54325999611122067",
+      longitude: "-44.284021668688126",
+    },
+    street: "Rua Machado De Assis",
+  },
+
+  {
+    district: "Liberdade",
+    created_at: "2025-03-13T07:51:00.000Z",
+    falseId: "2025-03-13T07:51:00.000Z",
+    status: 0,
+    geohash: "7p8986c",
+    subregion: "São Luís",
+    childrens: [
+      {
+        severity: 1,
+        created_at: "2025-03-13T07:51:00.000Z",
+      },
+    ],
+    address: "São Luís_Liberdade",
+    id: "generated_1",
+    coordinates: {
+      latitude: "-2.5336",
+      longitude: "-44.2845",
+    },
+    street: "Rua Machado De Assis",
+  },
+  {
+    district: "Liberdade",
+    created_at: "2025-03-13T07:51:00.000Z",
+    falseId: "2025-03-13T07:51:00.000Z",
+    status: 0,
+    geohash: "7p8986c",
+    subregion: "São Luís",
+    childrens: [
+      {
+        severity: 1,
+        created_at: "2025-03-13T07:51:00.000Z",
+      },
+    ],
+    address: "São Luís_Liberdade",
+    id: "generated_2",
+    coordinates: {
+      latitude: "-2.5351",
+      longitude: "-44.2838",
+    },
+    street: "Rua Machado De Assis",
+  },
+  {
+    district: "Liberdade",
+    created_at: "2025-03-13T07:51:00.000Z",
+    falseId: "2025-03-13T07:51:00.000Z",
+    status: 0,
+    geohash: "7p8986c",
+    subregion: "São Luís",
+    childrens: [
+      {
+        severity: 1,
+        created_at: "2025-03-13T07:51:00.000Z",
+      },
+    ],
+    address: "São Luís_Liberdade",
+    id: "generated_3",
+    coordinates: {
+      latitude: "-2.5371",
+      longitude: "-44.2833",
+    },
+    street: "Rua Machado De Assis",
+  },
 ];
